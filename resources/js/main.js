f61286305481a3d490e525e0a1a86409d1697dd6
Zaymus <carson.brown47@gmail.com>
2021-09-23 23:34:55 -0400

    project section links interactivity

--- resources/js/main.js
+++ resources/js/main.js
@@ -11,7 +11,10 @@ const location_techStack = document.querySelector("#languages").offsetTop;
 const location_projects = document.querySelector("#projects").offsetTop;
 const location_contact = document.querySelector("#contact").offsetTop;
 
-console.log(location_techStack);
+const projects_git = document.querySelectorAll("#project-git");
+const projects_web = document.querySelectorAll("#project-link");
+const git_links = ["https://github.com/Zaymus/Flexbox_Landing_Page", "https://github.com/Zaymus/user-login-register-webpage", "https://github.com/Zaymus/Tea-Cozy"];
+const web_links = ["https://zaymus.github.io/Flexbox_Landing_Page/", "https://zaymus.github.io/user-login-register-webpage/", "https://zaymus.github.io/Tea-Cozy/"];
 
 linkedin.addEventListener("click", () => {
     window.open("https://www.linkedin.com/in/carsonbrown-dev", "_blank");
@@ -30,13 +33,30 @@ resume.addEventListener("click", () => {
 });
 
 techStack.addEventListener("click", () => {
+    console.log("Languages clicked");
     scroll({top: location_techStack, behaviour: "smooth"});
 });
 
 projects.addEventListener("click", () => {
+    console.log("Projects clicked");
     scroll({top: location_projects, behaviour: "smooth"});
 });
 
 contact.addEventListener("click", () => {
+    console.log("Contact clicked");
     scroll({top: location_contact, behaviour: "smooth"});
 });
+
+for(let i = 0; i < git_links.length; i++)
+{
+    projects_git[i].addEventListener("click", () => {
+        window.open(git_links[i], "_blank");
+    });
+}
+
+for(let i = 0; i < web_links.length; i++)
+{
+    projects_web[i].addEventListener("click", () => {
+        window.open(web_links[i], "_blank");
+    });
+}
