12a5acb6165fc3fd7b8bc850ea331c25b2614fc2
Zaymus <carson.brown47@gmail.com>
2021-09-24 20:21:40 -0400

    added language body section

--- resources/js/pop-up.js
+++ resources/js/pop-up.js
@@ -2,7 +2,9 @@ const popup = document.querySelector(".pop_up-container");
 const popup_img = document.querySelector(".pop_up").childNodes[1].childNodes[1];
 const popup_box = document.getElementById("pop_up-row");
 const popup_title = document.querySelector(".pop_up-title");
+const popup_body = document.querySelector(".pop_up-body");
 const languages = document.querySelectorAll(".language");
+const language_bodies = document.querySelectorAll(".language_body");
 const images = document.querySelectorAll(".language_img--wrapper");
 const popup_close = document.querySelector(".pop_up-close");
 
@@ -15,28 +17,34 @@ const language_clicked = (event) => {
     var imageSrc = "";
     var imageAlt = "";
     var title = "";
+    var body = "";
 
     if(event.target.nodeName == "IMG")
     {
         imageSrc = event.target.src;
         imageAlt = event.target.alt;
         title = event.target.parentNode.parentNode.childNodes[3].innerHTML;
+        body = event.target.parentNode.parentNode.childNodes[5].defaultValue;
     }
     else if (event.target.nodeName == "FIGURE")
     {
         imageSrc = event.target.childNodes[1].src;
         imageAlt = event.target.childNodes[1].alt;
         title = event.target.parentNode.childNodes[3].innerHTML;
+        body = event.target.parentNode.childNodes[5].defaultValue;
     }
     else if (event.target.nodeName == "DIV")
     {
         imageSrc = event.target.childNodes[1].childNodes[1].src;
         imageAlt = event.target.childNodes[1].childNodes[1].alt;
         title = event.target.childNodes[3].innerHTML;
+        body = event.target.childNodes[5].defaultValue;
     }
     popup_img.src = imageSrc;
     popup_img.alt = imageAlt;
     popup_title.innerHTML = title;
+    if (body == ""){body = "Language details comming soon!"}
+    popup_body.innerHTML = body;
     popup.style.display = "block"
     setTimeout(startAnimation, 1);
 };
@@ -46,7 +54,6 @@ const startAnimation = () => {
     popup.style.display = "block";
     popup.classList.add("animate");
     popup_box.classList.add("animate");
-    // setInterval(function(){popup_box.classList.add("animate")}, 500);
 };
 
 // left: 37, up: 38, right: 39, down: 40,
@@ -125,4 +132,3 @@ popup.onclick = (event) => {
 popup_close.onclick = () => {
     close_popup();
 }
-
