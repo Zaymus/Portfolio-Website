a2b9f897b611f00d444320335a9299ae4e329a6c
Zaymus <carson.brown47@gmail.com>
2021-09-24 20:28:01 -0400

    formatting

--- resources/js/main.js
+++ resources/js/main.js
@@ -13,50 +13,56 @@ const location_contact = document.querySelector("#contact").offsetTop;
 
 const projects_git = document.querySelectorAll("#project-git");
 const projects_web = document.querySelectorAll("#project-link");
-const git_links = ["https://github.com/Zaymus/Flexbox_Landing_Page", "https://github.com/Zaymus/user-login-register-webpage", "https://github.com/Zaymus/Tea-Cozy"];
-const web_links = ["https://zaymus.github.io/Flexbox_Landing_Page/", "https://zaymus.github.io/user-login-register-webpage/", "https://zaymus.github.io/Tea-Cozy/"];
+const git_links = [
+	"https://github.com/Zaymus/Flexbox_Landing_Page",
+	"https://github.com/Zaymus/user-login-register-webpage",
+	"https://github.com/Zaymus/Tea-Cozy",
+];
+const web_links = [
+	"https://zaymus.github.io/Flexbox_Landing_Page/",
+	"https://zaymus.github.io/user-login-register-webpage/",
+	"https://zaymus.github.io/Tea-Cozy/",
+];
 
 linkedin.addEventListener("click", () => {
-    window.open("https://www.linkedin.com/in/carsonbrown-dev", "_blank");
+	window.open("https://www.linkedin.com/in/carsonbrown-dev", "_blank");
 });
 
 github.addEventListener("click", () => {
-    window.open("https://github.com/Zaymus", "_blank");
+	window.open("https://github.com/Zaymus", "_blank");
 });
 
 email.addEventListener("click", () => {
-    window.open("mailto:carsonbrown47@gmail.com", "_blank");
+	window.open("mailto:carsonbrown47@gmail.com", "_blank");
 });
 
 resume.addEventListener("click", () => {
-    window.open("./resources/assets/Resume.pdf", "_blank");
+	window.open("./resources/assets/Resume.pdf", "_blank");
 });
 
 techStack.addEventListener("click", () => {
-    console.log("Languages clicked");
-    scroll({top: location_techStack, behaviour: "smooth"});
+	console.log("Languages clicked");
+	scroll({ top: location_techStack, behaviour: "smooth" });
 });
 
 projects.addEventListener("click", () => {
-    console.log("Projects clicked");
-    scroll({top: location_projects, behaviour: "smooth"});
+	console.log("Projects clicked");
+	scroll({ top: location_projects, behaviour: "smooth" });
 });
 
 contact.addEventListener("click", () => {
-    console.log("Contact clicked");
-    scroll({top: location_contact, behaviour: "smooth"});
+	console.log("Contact clicked");
+	scroll({ top: location_contact, behaviour: "smooth" });
 });
 
-for(let i = 0; i < git_links.length; i++)
-{
-    projects_git[i].addEventListener("click", () => {
-        window.open(git_links[i], "_blank");
-    });
+for (let i = 0; i < git_links.length; i++) {
+	projects_git[i].addEventListener("click", () => {
+		window.open(git_links[i], "_blank");
+	});
 }
 
-for(let i = 0; i < web_links.length; i++)
-{
-    projects_web[i].addEventListener("click", () => {
-        window.open(web_links[i], "_blank");
-    });
+for (let i = 0; i < web_links.length; i++) {
+	projects_web[i].addEventListener("click", () => {
+		window.open(web_links[i], "_blank");
+	});
 }
--- resources/js/pop-up.js
+++ resources/js/pop-up.js
@@ -11,124 +11,125 @@ const popup_close = document.querySelector(".pop_up-close");
 popup.style.display = "none";
 
 const language_clicked = (event) => {
-    languages.forEach((language) => {
-        language.removeEventListener("click", language_clicked);
-    });
-    var imageSrc = "";
-    var imageAlt = "";
-    var title = "";
-    var body = "";
+	languages.forEach((language) => {
+		language.removeEventListener("click", language_clicked);
+	});
+	var imageSrc = "";
+	var imageAlt = "";
+	var title = "";
+	var body = "";
 
-    if(event.target.nodeName == "IMG")
-    {
-        imageSrc = event.target.src;
-        imageAlt = event.target.alt;
-        title = event.target.parentNode.parentNode.childNodes[3].innerHTML;
-        body = event.target.parentNode.parentNode.childNodes[5].defaultValue;
-    }
-    else if (event.target.nodeName == "FIGURE")
-    {
-        imageSrc = event.target.childNodes[1].src;
-        imageAlt = event.target.childNodes[1].alt;
-        title = event.target.parentNode.childNodes[3].innerHTML;
-        body = event.target.parentNode.childNodes[5].defaultValue;
-    }
-    else if (event.target.nodeName == "DIV")
-    {
-        imageSrc = event.target.childNodes[1].childNodes[1].src;
-        imageAlt = event.target.childNodes[1].childNodes[1].alt;
-        title = event.target.childNodes[3].innerHTML;
-        body = event.target.childNodes[5].defaultValue;
-    }
-    popup_img.src = imageSrc;
-    popup_img.alt = imageAlt;
-    popup_title.innerHTML = title;
-    if (body == ""){body = "Language details comming soon!"}
-    popup_body.innerHTML = body;
-    popup.style.display = "block"
-    setTimeout(startAnimation, 1);
+	if (event.target.nodeName == "IMG") {
+		imageSrc = event.target.src;
+		imageAlt = event.target.alt;
+		title = event.target.parentNode.parentNode.childNodes[3].innerHTML;
+		body = event.target.parentNode.parentNode.childNodes[5].defaultValue;
+	} else if (event.target.nodeName == "FIGURE") {
+		imageSrc = event.target.childNodes[1].src;
+		imageAlt = event.target.childNodes[1].alt;
+		title = event.target.parentNode.childNodes[3].innerHTML;
+		body = event.target.parentNode.childNodes[5].defaultValue;
+	} else if (event.target.nodeName == "DIV") {
+		imageSrc = event.target.childNodes[1].childNodes[1].src;
+		imageAlt = event.target.childNodes[1].childNodes[1].alt;
+		title = event.target.childNodes[3].innerHTML;
+		body = event.target.childNodes[5].defaultValue;
+	}
+	popup_img.src = imageSrc;
+	popup_img.alt = imageAlt;
+	popup_title.innerHTML = title;
+	if (body == "") {
+		body = "Language details comming soon!";
+	}
+	popup_body.innerHTML = body;
+	popup.style.display = "block";
+	setTimeout(startAnimation, 1);
 };
 
 const startAnimation = () => {
-    disableScroll();
-    popup.style.display = "block";
-    popup.classList.add("animate");
-    popup_box.classList.add("animate");
+	disableScroll();
+	popup.style.display = "block";
+	popup.classList.add("animate");
+	popup_box.classList.add("animate");
 };
 
 // left: 37, up: 38, right: 39, down: 40,
 // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
-const keys = {37: 1, 38: 1, 39: 1, 40: 1};
+const keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
 
 function preventDefault(e) {
-  e.preventDefault();
+	e.preventDefault();
 }
 
 function preventDefaultForScrollKeys(e) {
-  if (keys[e.keyCode]) {
-    preventDefault(e);
-    return false;
-  }
+	if (keys[e.keyCode]) {
+		preventDefault(e);
+		return false;
+	}
 }
 
 // modern Chrome requires { passive: false } when adding event
 var supportsPassive = false;
 try {
-  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
-    get: function () { supportsPassive = true; } 
-  }));
-} catch(e) {}
+	window.addEventListener(
+		"test",
+		null,
+		Object.defineProperty({}, "passive", {
+			get: function () {
+				supportsPassive = true;
+			},
+		})
+	);
+} catch (e) {}
 
 var wheelOpt = supportsPassive ? { passive: false } : false;
-var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+var wheelEvent =
+	"onwheel" in document.createElement("div") ? "wheel" : "mousewheel";
 
 const disableScroll = () => {
-    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
-    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
-    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
-    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
-}
+	window.addEventListener("DOMMouseScroll", preventDefault, false); // older FF
+	window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+	window.addEventListener("touchmove", preventDefault, wheelOpt); // mobile
+	window.addEventListener("keydown", preventDefaultForScrollKeys, false);
+};
 
 const enableScroll = () => {
-    window.removeEventListener('DOMMouseScroll', preventDefault, false);
-    window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
-    window.removeEventListener('touchmove', preventDefault, wheelOpt);
-    window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
-}
+	window.removeEventListener("DOMMouseScroll", preventDefault, false);
+	window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+	window.removeEventListener("touchmove", preventDefault, wheelOpt);
+	window.removeEventListener("keydown", preventDefaultForScrollKeys, false);
+};
 
 const removeDisplay = () => {
-    popup.style.display = "none";
-    enableScroll();
-    languages.forEach((language) => {
-        language.addEventListener("click", language_clicked);
-    });
+	popup.style.display = "none";
+	enableScroll();
+	languages.forEach((language) => {
+		language.addEventListener("click", language_clicked);
+	});
 };
 
 const close_popup = () => {
-    popup.classList.remove("animate");
-    popup_box.classList.remove("animate");
-    setTimeout(removeDisplay, 800);
-}
-
+	popup.classList.remove("animate");
+	popup_box.classList.remove("animate");
+	setTimeout(removeDisplay, 800);
+};
 
 languages.forEach((language) => {
-    language.addEventListener("click", language_clicked);
+	language.addEventListener("click", language_clicked);
 });
 
-document.addEventListener('keydown', (e) => {
-    if(e.which == 27)
-    {
-        close_popup();
-    }
+document.addEventListener("keydown", (e) => {
+	if (e.which == 27) {
+		close_popup();
+	}
 });
 
 popup.onclick = (event) => {
-    if(event.path[0].className.includes("pop_up-container"))
-    {
-        close_popup();
-    }
+	if (event.path[0].className.includes("pop_up-container")) {
+		close_popup();
+	}
 };
 
 popup_close.onclick = () => {
-    close_popup();
-}
+	close_popup();
+};
